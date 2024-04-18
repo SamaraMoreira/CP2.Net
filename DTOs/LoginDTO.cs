7c830808ee9b4e0f2d96600dbd6691838fd0dbda
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-04-18 01:17:45 -0300

    Adicionando arquivos e controlador de login

---
+++ DTOs/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CP2.Net.DTOs
+{
+    public class LoginDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
